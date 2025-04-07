21192f649e5def0a002a4b7c15626f16ea946c5c
Vanessa Martins <124753005+vanimartins67@users.noreply.github.com>
2025-04-06 20:13:42 -0400

    lista basicos 10-04

---
+++ exercicios_lista_basico_10-04.py
@@ -0,0 +1,68 @@
+#exercícios de acesso aos elementos
+
+#1
+numeros = [1, 2, 3, 4, 5]
+print.type(numeros)
+#2
+nomes = ["Vani", "Namtan", "Hyeri", "Faye"]
+print.type(nomes)
+#3
+vazia = []
+vazia.append(10, 20, 30)
+print(vazia)
+#4
+cinco = ["a", "b", "c", "d", "e"]
+print(cinco[0])
+#5
+tres =["a", "b", "c"]
+print(tres[2])
+#6
+dez = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(dez[3])
+#7
+seis = [1, 2, 3, 4, 5, 6]
+print(seis[2, 6])
+#8
+cidades = ["Campo Grande", "São Paulo", "Belo Horzonte"]
+print(cidades[2])
+#9
+cores = ["preto", "azul", "verde", "amarelo", "branco"]
+print(cores[1])
+#10
+cinco = [1, 2, 3, 4, 5]
+print(cinco)
+#11
+oito = [1, 2, 3, 4, 5, 6, 7, 8]
+print(oito[0:4])
+#12
+sete = [1, 2, 3, 4, 5, 6, 7]
+print(sete[3:8])
+#13
+cinco = [1, 2, 3, 4, 5]
+print(cinco[1:5])
+#14
+frutas = ["banana", "maçã", "laranja", "uva", "abacaxi"]
+print(frutas)
+print(frutas)
+#15
+numeros = [1, 2, 3]
+letras = ["a", "b", "c"]
+print(numeros + letras)
+#16
+seis = [1, 2, 3, 4, 5, 6]
+print.len(seis)
+#17
+dez = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(dez.count)
+#18
+lista = [1, 2, 3, 4, 5]    
+print(lista.index(3))
+#19
+somar = [1, 2, 3, 4, 5]
+print(sum(somar))
+#20
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(max(lista))
+
+
+
