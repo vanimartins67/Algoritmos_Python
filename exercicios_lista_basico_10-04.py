8228cfda13ce378861b35682af8d90bdaa6ced28
Vanessa Martins <vmartins.ti@gmail.com>
2025-04-06 21:24:43 -0400

    Update exercicios_lista_basico_10-04.py

--- exercicios_lista_basico_10-04.py
+++ exercicios_lista_basico_10-04.py
@@ -2,13 +2,13 @@
 
 #1
 numeros = [1, 2, 3, 4, 5]
-print.type(numeros)
+print(numeros)
 #2
 nomes = ["Vani", "Namtan", "Hyeri", "Faye"]
-print.type(nomes)
+print(nomes)
 #3
 vazia = []
-vazia.append(10, 20, 30)
+vazia.extend([10, 20, 30])
 print(vazia)
 #4
 cinco = ["a", "b", "c", "d", "e"]
@@ -18,12 +18,12 @@ tres =["a", "b", "c"]
 print(tres[2])
 #6
 dez = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(dez[3])
+print(dez[2])
 #7
 seis = [1, 2, 3, 4, 5, 6]
-print(seis[2, 6])
+print(seis[1],",",seis[4])
 #8
-cidades = ["Campo Grande", "São Paulo", "Belo Horzonte"]
+cidades = ["Campo Grande", "São Paulo", "Belo Horizonte"]
 print(cidades[2])
 #9
 cores = ["preto", "azul", "verde", "amarelo", "branco"]
@@ -33,30 +33,30 @@ cinco = [1, 2, 3, 4, 5]
 print(cinco)
 #11
 oito = [1, 2, 3, 4, 5, 6, 7, 8]
-print(oito[0:4])
+print(oito[:4])
 #12
 sete = [1, 2, 3, 4, 5, 6, 7]
-print(sete[3:8])
+print(sete[-3:])
 #13
 cinco = [1, 2, 3, 4, 5]
-print(cinco[1:5])
+print(cinco[1:4])
 #14
 frutas = ["banana", "maçã", "laranja", "uva", "abacaxi"]
-print(frutas)
-print(frutas)
+print(frutas * 2)
 #15
 numeros = [1, 2, 3]
 letras = ["a", "b", "c"]
 print(numeros + letras)
 #16
 seis = [1, 2, 3, 4, 5, 6]
-print.len(seis)
+print(len(seis))
 #17
 dez = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(dez.count)
+print(len(dez))
 #18
-lista = [1, 2, 3, 4, 5]    
-print(lista.index(3))
+lista = [1, 2, 3, 4, 5]
+tamanho = len(lista)    
+print(tamanho)
 #19
 somar = [1, 2, 3, 4, 5]
 print(sum(somar))
