5cfc2bb065d4456420101fc36c4b0f8794c8ac1d
vanimartins67 <vmartins.ti@gmail.com>
2025-05-26 21:54:19 -0400

    Create aula 26-05.py

---
+++ aula 26-05.py
@@ -0,0 +1,18 @@
+# f = open("file.txt", "x")
+
+# f = open("file.txt" , "w")
+
+# f = open("file.txt", "r")
+# print(f.read())
+# f.close()
+
+# z = open("file.txt", "a", encoding="utf-8")
+# x = input("Digite seu nome = ")
+# z.write(x)
+# z = open("file.txt", "r")
+# print(z.read())
+# z.close()
+
+f = open("hello_kitty.png", "rb")
+print(f.read())
+
